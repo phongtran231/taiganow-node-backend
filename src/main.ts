a84d7707a223f6fce1777ab2bc842351d1c0dd5c
phongtran <phong@sleek.com>
2024-04-23 20:22:08 +0700

    init

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3001, '0.0.0.0', () =>
-    console.log('Server is running on http://localhost:3001'),
+  const config = app.get(ConfigService);
+  await app.listen(config.get('PORT'), '0.0.0.0', () =>
+    console.log(`Server is running on http://localhost:${config.get('PORT')}`),
   );
 }
 bootstrap();
